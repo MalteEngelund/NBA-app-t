431ab5c5be32d828d2c38b0b1e3e24ea4fbf1a86
MalteEngelund <malte.engelund@hotmail.com>
2026-04-14 12:25:22 +0200

    added initial serviceworker

--- root/src/App.tsx
+++ root/src/App.tsx
@@ -9,6 +9,12 @@ import { PlayerPage } from './pages/PlayerPage'
 
 function App() {
 
+  if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('service worker registered', reg))
+    .catch(err => console.error('service worker not registered', err)) 
+  }
+
   return (
     <>
       <BrowserRouter>
